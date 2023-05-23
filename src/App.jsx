c9aeda599e1e110b8db854915a20ad3adaf2d894
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-05-23 13:38:44 -0300

    agregamos el maquetado de app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Container} from "react-bootstrap";
 
 function App() {
-
   return (
     <>
-      <h1>caca</h1>
+      <Container className="my-5 mainPage">
+        <h1 className="text-center display-5 text-light">Lista Tareas</h1>
+        <hr className="text-light" />
+      </Container>
+      <footer className="py-4 bg-dark text-light ">
+        <p className="text-center">&copy;Todos los derechos reservados</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
