e7e398eef4f6b584ee81107fcfc81717a885a8bc
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-05-23 15:56:04 -0300

    agregue un boton a item-tareas

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,8 +1,11 @@
-import { ListGroup } from "react-bootstrap";
+import { Button, ListGroup } from "react-bootstrap";
 
 const ItemTarea = () => {
     return (
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item className="d-flex justify-content-between">
+            Cras justo odio
+            <Button variant="danger">Borrar</Button>
+        </ListGroup.Item>
     );
 };
 
