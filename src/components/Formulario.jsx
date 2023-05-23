0323230ee4e23c5a94a8f7a7fd2ae393f7a17789
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-05-23 20:15:59 -0300

    le di otro color al boton del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,15 +1,21 @@
-import {Form, Button} from 'react-bootstrap';
-import ListaTareas from './ListaTareas';
+import { Form, Button } from "react-bootstrap";
+import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const Formulario = () => {
+  const [tarea, setTarea] = useState("");
+  const [lista, setLista] = useState([]);
   return (
     <section>
       <Form>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
-          <Form.Control type="email" placeholder="Ingrese una tarea" />
+          <Form.Control type="text" placeholder="Ingrese una tarea" 
+          onChange={(e) => setTarea(e.target.value)}
+          value={tarea}
+          />
           <Button variant="primary" type="submit">
-          Enviar
-        </Button>
+            Enviar
+          </Button>
         </Form.Group>
       </Form>
       <ListaTareas></ListaTareas>
